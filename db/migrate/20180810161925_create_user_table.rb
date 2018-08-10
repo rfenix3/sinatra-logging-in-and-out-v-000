0554a91419545f13bade59d7c5c6df09ae02896a
Richard Fenix <richard.fenix@gmail.com>
2018-08-10 16:20:31 +0000

    automatically backed up by learn

--- db/migrate/20180810161925_create_user_table.rb
+++ db/migrate/20180810161925_create_user_table.rb
@@ -1,4 +1,10 @@
 class CreateUserTable < ActiveRecord::Migration[5.1]
   def change
+    create_table :users do |t|
+      t.string :username
+      t.string :password
+      t.integer :balance
+    end
+    
   end
 end
