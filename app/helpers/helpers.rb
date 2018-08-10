28a999cc572b371797fb3acf35114ab0216d969b
Richard Fenix <richard.fenix@gmail.com>
2018-08-10 17:16:15 +0000

    automatically backed up by learn

--- app/helpers/helpers.rb
+++ app/helpers/helpers.rb
@@ -1,11 +1,11 @@
 class Helpers
-  def self.current_user(session)
-    @user = User.find_by(id: session[:id])
+  def self.current_user(session_hash)
+    @user = User.find_by_id(id: session_hash[:id])
     @user
   end
 
-  def self.is_logged_in?(session)
-    !!User.find(session[:id])
+  def self.is_logged_in?(session_hash)
+    !!User.find(session_hash[:id])
   end
 
 end
